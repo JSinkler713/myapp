c738ed97d555bc304f0490dde1e9515beac16a58
James Sinkler <jamessinkler713@gmail.com>
2020-09-27 15:59:17 -0700

    destructures props; removes old data

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -2,37 +2,19 @@ import React from 'react';
 import { Text, StyleSheet, FlatList } from 'react-native';
 import ColorBox from '../components/ColorBox';
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
-
 const ColorPalette = (props) => {
   console.log(props)
   console.log(props.route.params.colors)
+  const { paletteName, colors } = props.route.params
   return (
     <FlatList
       style={styles.container}
-      data={props.route.params.colors}
+      data={colors}
       keyExtractor={item => item.hexCode}
       renderItem={({ item }) => (
         <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
       )}
-      ListHeaderComponent={<Text style={styles.heading}>{props.route.params.paletteName}</Text>}
+      ListHeaderComponent={<Text style={styles.heading}>{paletteName}</Text>}
     />
   );
 };
@@ -40,6 +22,7 @@ const ColorPalette = (props) => {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    backgroundColor: 'white',
   },
   heading: {
     fontSize: 18,
