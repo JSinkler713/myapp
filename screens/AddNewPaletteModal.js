4ca2345a24d8f79b8658af1df10a4f8a97aac316
James Sinkler <jamessinkler713@gmail.com>
2020-09-27 20:27:18 -0700

    adds new screen

---
+++ screens/AddNewPaletteModal.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect, useContext} from 'react'; 
+import { View, Text } from 'react-native'
+ 
+const AddNewPaletteModal = ()=> { 
+  return ( 
+     <View>
+       <Text> Check Check</Text>
+     </View> 
+  ) 
+} 
+export default AddNewPaletteModal;
